9750edfcba20aa610ce33e85b94cff5159f46d97
Joe Todd <joe.todd@sonos.com>
2022-07-12 14:52:00 +0100

    Add macOS arm64 cross compile script

---
+++ mac_arm64_build.sh
@@ -0,0 +1,97 @@
+OGGVERSION=1.3.5
+VORBISVERSION=1.3.7
+FLACVERSION=1.3.3
+OPUSVERSION=1.3.1
+MPG123VERSION=1.29.3
+LAMEVERSION=3.100
+SNDFILE_VERSION=1.0.31
+
+JOBS=8
+SNDFILENAME=libsndfile-$SNDFILE_VERSION
+OGG_INCDIR="$(pwd)/libogg-$OGGVERSION/include"
+OGG_LIBDIR="$(pwd)/libogg-$OGGVERSION/src/.libs"
+
+export MACOSX_DEPLOYMENT_TARGET=11.0
+XCORR_CONF="--host=aarch64-apple-darwin"
+XCORR_FLAGS="-arch arm64 -target arm64-apple-macos11"
+
+# libogg
+
+curl -LO https://downloads.xiph.org/releases/ogg/libogg-$OGGVERSION.tar.gz
+tar zxvf libogg-$OGGVERSION.tar.gz
+cd libogg-$OGGVERSION
+CFLAGS="$XCORR_FLAGS" CXXFLAGS="$XCORR_FLAGS" ./configure --host=aarch64-apple-darwin --disable-shared
+make -j$JOBS
+cd ..
+
+# libvorbis
+
+curl -LO https://downloads.xiph.org/releases/vorbis/libvorbis-$VORBISVERSION.tar.gz
+tar zxvf libvorbis-$VORBISVERSION.tar.gz
+cd libvorbis-$VORBISVERSION
+CFLAGS="$XCORR_FLAGS" CXXFLAGS="$XCORR_FLAGS" ./configure --host=aarch64-apple-darwin --disable-shared --with-ogg-includes=$OGG_INCDIR --with-ogg-libraries=$OGG_LIBDIR
+make -j$JOBS
+cd ..
+
+# libFLAC
+
+curl -LO https://downloads.xiph.org/releases/flac/flac-$FLACVERSION.tar.xz
+unxz flac-$FLACVERSION.tar.xz
+tar zxvf flac-$FLACVERSION.tar
+cd flac-$FLACVERSION
+CFLAGS="$XCORR_FLAGS" CXXFLAGS="$XCORR_FLAGS" ./configure --host=aarch64-apple-darwin --enable-static --disable-shared --with-ogg-includes=$OGG_INCDIR --with-ogg-libraries=$OGG_LIBDIR
+make -j$JOBS
+cd ..
+
+# libopus
+
+curl -LO https://archive.mozilla.org/pub/opus/opus-$OPUSVERSION.tar.gz
+tar zxvf opus-$OPUSVERSION.tar.gz
+cd opus-$OPUSVERSION
+CFLAGS="$XCORR_FLAGS" CXXFLAGS="$XCORR_FLAGS" ./configure --host=aarch64-apple-darwin --disable-shared
+make -j$JOBS
+cd ..
+
+# mpg123
+
+curl -LO https://deac-ams.dl.sourceforge.net/project/mpg123/mpg123/$MPG123VERSION/mpg123-$MPG123VERSION.tar.bz2
+tar zxvf mpg123-$MPG123VERSION.tar.bz2
+cd mpg123-$MPG123VERSION
+CFLAGS="$XCORR_FLAGS" CXXFLAGS="$XCORR_FLAGS" ./configure --host=aarch64-apple-darwin --enable-static --disable-shared
+make -j$JOBS
+cd ..
+
+# liblame
+
+curl -LO https://deac-ams.dl.sourceforge.net/project/lame/lame/$LAMEVERSION/lame-$LAMEVERSION.tar.gz
+tar zxvf lame-$LAMEVERSION.tar.gz
+cd lame-$LAMEVERSION
+CFLAGS="$XCORR_FLAGS" CXXFLAGS="$XCORR_FLAGS" ./configure --host=aarch64-apple-darwin --enable-static --disable-shared
+make -j$JOBS
+cd ..
+
+# libsndfile
+
+export FLAC_CFLAGS="-I$(pwd)/flac-$FLACVERSION/include"
+export FLAC_LIBS="$(pwd)/flac-$FLACVERSION/src/libFLAC/libFLAC.la"
+export OGG_CFLAGS="-I$(pwd)/libogg-$OGGVERSION/include"
+export OGG_LIBS="$(pwd)/libogg-$OGGVERSION/src/libogg.la"
+export VORBIS_CFLAGS="-I$(pwd)/libvorbis-$VORBISVERSION/include"
+export VORBIS_LIBS="$(pwd)/libvorbis-$VORBISVERSION/lib/libvorbis.la"
+export VORBISENC_CFLAGS="-I$(pwd)/libvorbis-$VORBISVERSION/include"
+export VORBISENC_LIBS="$(pwd)/libvorbis-$VORBISVERSION/lib/libvorbisenc.la"
+export OPUS_CFLAGS="-I$(pwd)/opus-$OPUSVERSION/include"
+export OPUS_LIBS="$(pwd)/opus-$OPUSVERSION/libopus.la"
+# apparently, the MPG123-variables are used for both MPG123 and lame
+export MPG123_CFLAGS="-I$(pwd)/mpg123-$MPG123VERSION/src/libmpg123/ -I$(pwd)/lame-$LAMEVERSION/include"
+export MPG123_LIBS="$(pwd)/mpg123-$MPG123VERSION/src/libmpg123/libmpg123.la $LAME_LIBS$(pwd)/lame-$LAMEVERSION/libmp3lame/.libs/libmp3lame.la"
+
+curl -LO https://github.com/libsndfile/libsndfile/releases/download/$SNDFILE_VERSION/$SNDFILENAME.tar.bz2
+tar jxvf $SNDFILENAME.tar.bz2
+cd $SNDFILENAME
+CFLAGS="$XCORR_FLAGS" CXXFLAGS="$XCORR_FLAGS" ./configure --host=aarch64-apple-darwin --disable-static --disable-sqlite --disable-alsa
+make -j$JOBS
+cd ..
+
+cp $SNDFILENAME/src/.libs/libsndfile.1.dylib libsndfile-arm64.dylib
+chmod -x libsndfile-arm64.dylib
